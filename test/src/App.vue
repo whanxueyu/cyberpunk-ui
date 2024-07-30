01302eb9ec088cb413f9caf735738da8ecb89013
anxueyu <1358042645@qq.com>
2024-07-30 18:59:42 +0800

    feat：组件安装测试成功

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
   <div>
-
+    <h1>测试功能</h1>
+    <cp-button>测试按钮</cp-button>
   </div>
 </template>
 
--- test/src/main.ts
+++ test/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import CyberpunkUI from 'cyberpunk-ui';
+import 'cyberpunk-ui/lib/style.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(CyberpunkUI)
+app.mount('#app')
