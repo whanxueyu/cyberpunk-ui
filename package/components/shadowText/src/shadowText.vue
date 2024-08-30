5faddab15a493747454939078de0ee76d6f7dc18
anxueyu <1358042645@qq.com>
2024-08-30 11:55:56 +0800

    fix：修复阴影文字无法修改阴影颜色和阴影长度参数的问题

--- package/components/shadowText/src/shadowText.vue
+++ package/components/shadowText/src/shadowText.vue
@@ -1,34 +1,47 @@
 <template>
-    <div :class="['shadow-text', props.type]">
+    <div :class="['shadow-text', props.direction]" :style="{ textShadow: makeLongShadow(props.shadowColor, props.direction) }">
         <slot></slot>
     </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
 defineOptions({
     name: 'CpShadowText',
 })
 const props = defineProps({
-    type: {
+    direction: {
         type: String,
         default: 'left',
-        validator: (value: string) => {
-            return ['left', 'right'].indexOf(value) !== -1
-        }
     },
     shadowColor: {
         type: String,
         default: '#ae0ed68c',
     },
+    long:{
+        type: Number,
+        default: 20,
+    }
 })
-// onMounted(()=>{
-  document.documentElement.style.setProperty('--shadow-color', props.shadowColor)
-// })
+
+const makeLongShadow = (color,direction): string => {
+    let val = `0px 0px ${color}`;
+
+    for (let i = 1; i <= props.long; i++) {
+        const newColor = adjustColor(color, i);
+        val += `, ${direction === 'left' ? '-' : ''}${i}px ${i}px ${newColor}`;
+    }
+    return val;
+};
+const adjustColor = (color: string, step: number): string => {
+    // 这里简化了颜色调整逻辑，实际可能需要更复杂的算法来模拟fade-out和desaturate效果
+    return color;
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
+
+
 @function makelongleftshadow($color) {
     $val: 0px 0px $color;
 
@@ -51,16 +64,8 @@ const props = defineProps({
     @return $val;
 }
 
-.right {
-    text-shadow: makelongrightshadow(#008286);
-}
-
 .shadow-text {
     text-align: center;
     display: inline-block;
-}
-
-.left {
-    text-shadow: makelongleftshadow(#008286);
 }
 </style>
