15dedd461254d7a6d2785e44ffd520683293a33d
anxueyu <1358042645@qq.com>
2025-11-05 15:39:37 +0800

    feat: 打包上传

--- package/components/banner/src/banner.vue.js
+++ package/components/banner/src/banner.vue.js
@@ -5,27 +5,37 @@ defineOptions({
 const props = defineProps({
     imgList: {
         type: Array,
-        default: [
-            'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
-            'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg',
-            'https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg',
-            'https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg',
-            'https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg',
-            'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
-            'https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg',
-        ]
+        default: []
     },
-    translateZ: {
+    shadow: {
+        type: Boolean,
+        default: true
+    },
+    animationDuration: {
         type: Number,
-        default: 650
+        default: 30
+    },
+    imgWidth: {
+        type: Number,
+        default: 500
+    },
+    imgHeight: {
+        type: Number,
+        default: 400
     }
 });
-const imageCount = ref(1);
-imageCount.value = props.imgList.length;
+const imageCount = ref(props.imgList.length);
+const containerWidth = calculateWaistLength(props.imgWidth, 360 / imageCount.value) * 1.6;
+function calculateWaistLength(baseLength, apexAngle) {
+    const angleInRadians = apexAngle * Math.PI / 180;
+    const waistLength = baseLength / (2 * Math.sin(angleInRadians / 2));
+    return waistLength;
+}
 const getImageStyle = (index) => {
     const angle = 35 + (index + 1) * (360 / imageCount.value);
+    const waistLength = calculateWaistLength(props.imgWidth, 360 / imageCount.value);
     return {
-        transform: `rotateY(${angle}deg) translateZ(${props.translateZ}px)`
+        transform: `rotateY(${angle}deg) translateZ(${waistLength}px)`
     };
 };
 debugger;
@@ -33,13 +43,16 @@ const __VLS_ctx = {};
 let __VLS_components;
 let __VLS_directives;
 ;
-;
-;
-;
-__VLS_asFunctionalElement(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)(Object.assign({ class: "container" }));
-__VLS_asFunctionalElement(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)(Object.assign({ class: "stage" }));
-__VLS_asFunctionalElement(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)(Object.assign({ class: "control" }));
-__VLS_asFunctionalElement(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)(Object.assign({ class: "imgWrap" }));
+__VLS_asFunctionalElement(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)(Object.assign({ class: "container" }, { style: ({
+        '--container-width': `${__VLS_ctx.containerWidth}px`,
+        '--img-width': `${props.imgWidth}px`,
+        '--img-height': `${props.imgHeight}px`,
+    }) }));
+__VLS_asFunctionalElement(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)(Object.assign({ class: "stage" }, { class: ({ 'has-shadow': props.shadow }) }));
+__VLS_asFunctionalElement(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)(Object.assign({ class: "control" }, { style: ({
+        '--animation-duration': `${props.animationDuration}s`,
+    }) }));
+__VLS_asFunctionalElement(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)(Object.assign({ class: " imgWrap" }));
 for (const [img, index] of __VLS_getVForSourceType((props.imgList))) {
     __VLS_asFunctionalElement(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)(Object.assign(Object.assign({ key: (index) }, { class: "img" }), { style: (__VLS_ctx.getImageStyle(index)) }));
     __VLS_asFunctionalElement(__VLS_intrinsicElements.img, __VLS_intrinsicElements.img)({
@@ -51,29 +64,35 @@ for (const [img, index] of __VLS_getVForSourceType((props.imgList))) {
 ;
 ;
 ;
+;
 var __VLS_dollars;
 const __VLS_self = (await import('vue')).defineComponent({
     setup() {
         return {
+            containerWidth: containerWidth,
             getImageStyle: getImageStyle,
         };
     },
     props: {
         imgList: {
             type: Array,
-            default: [
-                'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
-                'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg',
-                'https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg',
-                'https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg',
-                'https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg',
-                'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
-                'https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg',
-            ]
+            default: []
         },
-        translateZ: {
+        shadow: {
+            type: Boolean,
+            default: true
+        },
+        animationDuration: {
             type: Number,
-            default: 650
+            default: 30
+        },
+        imgWidth: {
+            type: Number,
+            default: 500
+        },
+        imgHeight: {
+            type: Number,
+            default: 400
         }
     },
 });
@@ -84,19 +103,23 @@ export default (await import('vue')).defineComponent({
     props: {
         imgList: {
             type: Array,
-            default: [
-                'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
-                'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg',
-                'https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg',
-                'https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg',
-                'https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg',
-                'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
-                'https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg',
-            ]
+            default: []
         },
-        translateZ: {
+        shadow: {
+            type: Boolean,
+            default: true
+        },
+        animationDuration: {
             type: Number,
-            default: 650
+            default: 30
+        },
+        imgWidth: {
+            type: Number,
+            default: 500
+        },
+        imgHeight: {
+            type: Number,
+            default: 400
         }
     },
 });
--- package/components/image/src/image.vue.js
+++ package/components/image/src/image.vue.js
@@ -1,14 +1,80 @@
-import { defineProps } from 'vue';
+import { defineProps, withDefaults, ref, computed, onMounted, watch } from 'vue';
 defineOptions({
     name: 'CyberImage',
 });
-const props = defineProps({
-    imgurl: {
-        type: String,
-        default: 'https://mzz-files.oss-cn-shenzhen.aliyuncs.com///uploads/U1002433/0cb5e044a1f0f7fc15f61264ee97ac1f.png'
+const props = withDefaults(defineProps(), {
+    src: '',
+    alt: '',
+    width: '100%',
+    height: '100%',
+    fit: 'cover',
+    loading: 'lazy',
+    errorSrc: '',
+    crossorigin: 'anonymous',
+    errorContent: 'Image failed to load',
+    disableGlitch: false
+});
+const isLoaded = ref(false);
+const hasError = ref(false);
+const actualSrc = ref(props.src);
+const normalizedWidth = computed(() => typeof props.width === 'number' ? `${props.width}px` : props.width);
+const normalizedHeight = computed(() => typeof props.height === 'number' ? `${props.height}px` : props.height);
+const backgroundImage = computed(() => {
+    if (hasError.value && props.errorSrc)
+        return `url(${props.errorSrc}) no-repeat`;
+    return `url(${actualSrc.value}) no-repeat`;
+});
+const loadImage = () => {
+    if (!props.src)
+        return;
+    isLoaded.value = false;
+    hasError.value = false;
+    const img = new Image();
+    if (props.crossorigin)
+        img.crossOrigin = props.crossorigin;
+    img.src = props.src;
+    img.onload = () => {
+        actualSrc.value = props.src;
+        isLoaded.value = true;
+    };
+    img.onerror = () => {
+        hasError.value = true;
+        if (props.errorSrc && props.errorSrc !== props.src) {
+            actualSrc.value = props.errorSrc;
+            const errorImg = new Image();
+            errorImg.src = props.errorSrc;
+            errorImg.onload = () => isLoaded.value = true;
+        }
+        else {
+            isLoaded.value = true;
+        }
+    };
+};
+const backgroundSize = computed(() => {
+    switch (props.fit) {
+        case 'fill':
+            return '100% 100%';
+        case 'scale-down':
+            return 'contain';
+        default:
+            return props.fit;
     }
 });
+watch(() => props.src, loadImage);
+onMounted(loadImage);
 debugger;
+const __VLS_withDefaultsArg = (function (t) { return t; })({
+    src: '',
+    alt: '',
+    width: '100%',
+    height: '100%',
+    fit: 'cover',
+    loading: 'lazy',
+    errorSrc: '',
+    crossorigin: 'anonymous',
+    errorContent: 'Image failed to load',
+    disableGlitch: false
+});
 const __VLS_ctx = {};
 let __VLS_components;
 let __VLS_directives;
@@ -16,37 +82,70 @@ let __VLS_directives;
 ;
 ;
 ;
-__VLS_asFunctionalElement(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)(Object.assign({ class: "imgbox" }));
-__VLS_asFunctionalElement(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)(Object.assign({ class: "cyberimg" }, { style: ({
-        background: `url(${props.imgurl}) no-repeat`,
-        backgroundSize: 'cover'
+;
+;
+;
+__VLS_asFunctionalElement(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)({});
+__VLS_asFunctionalElement(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)(Object.assign(Object.assign({ class: "imgbox" }, { style: ({ width: __VLS_ctx.normalizedWidth, height: __VLS_ctx.normalizedHeight }) }), { role: "img", 'aria-label': (__VLS_ctx.alt || undefined), 'aria-busy': (!__VLS_ctx.isLoaded && !__VLS_ctx.hasError) }));
+if (__VLS_ctx.src) {
+    __VLS_asFunctionalElement(__VLS_intrinsicElements.img, __VLS_intrinsicElements.img)(Object.assign({ src: (__VLS_ctx.src), alt: (__VLS_ctx.alt), loading: (__VLS_ctx.loading), crossorigin: (__VLS_ctx.crossorigin) }, { class: "hidden-image" }));
+}
+__VLS_asFunctionalElement(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)(Object.assign(Object.assign({ class: "cyberimg" }, { class: ({
+        'loaded': __VLS_ctx.isLoaded && !__VLS_ctx.hasError,
+        'loading': !__VLS_ctx.isLoaded && !__VLS_ctx.hasError,
+        'error': __VLS_ctx.hasError,
+        'no-glitch': __VLS_ctx.disableGlitch
+    }) }), { style: ({
+        background: __VLS_ctx.backgroundImage,
+        backgroundSize: __VLS_ctx.backgroundSize
     }) }));
-var __VLS_0 = {};
+if (__VLS_ctx.isLoaded && !__VLS_ctx.hasError) {
+    var __VLS_0 = {};
+}
+else if (!__VLS_ctx.isLoaded && __VLS_ctx.placeholder) {
+    __VLS_asFunctionalElement(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)(Object.assign({ class: "placeholder" }));
+    var __VLS_2 = {};
+    __VLS_asFunctionalElement(__VLS_intrinsicElements.img, __VLS_intrinsicElements.img)({
+        src: (__VLS_ctx.placeholder),
+        alt: "Loading...",
+    });
+}
+else if (__VLS_ctx.hasError) {
+    __VLS_asFunctionalElement(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)(Object.assign({ class: "error" }));
+    var __VLS_4 = {};
+    (__VLS_ctx.errorContent);
+}
 ;
 ;
-var __VLS_1 = __VLS_0;
+;
+;
+;
+;
+;
+;
+;
+var __VLS_1 = __VLS_0, __VLS_3 = __VLS_2, __VLS_5 = __VLS_4;
 var __VLS_dollars;
 const __VLS_self = (await import('vue')).defineComponent({
     setup() {
-        return {};
+        return {
+            isLoaded: isLoaded,
+            hasError: hasError,
+            normalizedWidth: normalizedWidth,
+            normalizedHeight: normalizedHeight,
+            backgroundImage: backgroundImage,
+            backgroundSize: backgroundSize,
+        };
     },
-    props: {
-        imgurl: {
-            type: String,
-            default: 'https://mzz-files.oss-cn-shenzhen.aliyuncs.com///uploads/U1002433/0cb5e044a1f0f7fc15f61264ee97ac1f.png'
-        }
-    },
+    __typeProps: {},
+    props: {},
 });
 const __VLS_component = (await import('vue')).defineComponent({
     setup() {
         return {};
     },
-    props: {
-        imgurl: {
-            type: String,
-            default: 'https://mzz-files.oss-cn-shenzhen.aliyuncs.com///uploads/U1002433/0cb5e044a1f0f7fc15f61264ee97ac1f.png'
-        }
-    },
+    __typeProps: {},
+    props: {},
 });
 export default {};
 ;
