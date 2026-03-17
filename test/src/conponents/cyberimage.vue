b1d9f3177cd361ab353faf21602b28f8ed08bfef
anxueyu <1358042645@qq.com>
2026-03-17 17:15:18 +0800

    feat(image): 更新组件示例中的图片资源路径
    - 将远程CDN图片替换为本地assets目录下的cyber-building.jpeg
    - 修改所有cyber-image组件实例的src属性指向本地图片
    - 更新errorSrc属性使用本地图片资源
    - 统一所有图片演示示例使用相同的本地图片文件

--- test/src/conponents/cyberimage.vue
+++ test/src/conponents/cyberimage.vue
@@ -3,11 +3,11 @@
 
         <div>设置宽高 ----- width height loading </div>
         <div class="flex">
-            <cyber-image class="pic" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"/>
+            <cyber-image class="pic" src="/src/assets/img/cyber-building.jpeg"/>
             设置大小100*70
-            <cyber-image class="pic" width="100px" height="70px" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"/>
+            <cyber-image class="pic" width="100px" height="70px" src="/src/assets/img/cyber-building.jpeg"/>
             设置大小200*50
-            <cyber-image class="pic" fit="contain" width="200px" height="50px" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"/>
+            <cyber-image class="pic" fit="contain" width="200px" height="50px" src="/src/assets/img/cyber-building.jpeg"/>
         </div>
         <div>加载失败</div>
         <div class="flex">
@@ -15,25 +15,25 @@
             设置加载失败文字
             <cyber-image class="pic" src="555.png" errorContent="我尽力了，加载失败"/>
             设置加载失败后显示的图片
-            <cyber-image class="pic" src="555.png" errorSrc="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"/>
+            <cyber-image class="pic" src="555.png" errorSrc="/src/assets/img/cyber-building.jpeg"/>
         </div>
         <div>适应到容器框 ----- 'cover' | 'contain' |  'fill' | 'none' | 'scale-down'</div>
         <div class="flex">
-            <cyber-image fit="cover" class="picfit" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"/>
-            <cyber-image fit="contain" class="picfit" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"/>
-            <cyber-image fit="fill" class="picfit" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"/>
-            <cyber-image fit="none" class="picfit" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"/>
-            <cyber-image fit="scale-down" class="picfit" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"/>
+            <cyber-image fit="cover" class="picfit" src="/src/assets/img/cyber-building.jpeg"/>
+            <cyber-image fit="contain" class="picfit" src="/src/assets/img/cyber-building.jpeg"/>
+            <cyber-image fit="fill" class="picfit" src="/src/assets/img/cyber-building.jpeg"/>
+            <cyber-image fit="none" class="picfit" src="/src/assets/img/cyber-building.jpeg"/>
+            <cyber-image fit="scale-down" class="picfit" src="/src/assets/img/cyber-building.jpeg"/>
         </div>
 
         <div>加载 ----- lazy eager</div>
         <div class="flex">
-            <cyber-image loading="lazy" class="pic" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"/>
-            <cyber-image loading="eager" class="pic" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"/>
+            <cyber-image loading="lazy" class="pic" src="/src/assets/img/cyber-building.jpeg"/>
+            <cyber-image loading="eager" class="pic" src="/src/assets/img/cyber-building.jpeg"/>
             禁用效果
-            <cyber-image disableGlitch class="pic" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"/>
+            <cyber-image disableGlitch class="pic" src="/src/assets/img/cyber-building.jpeg"/>
 
-            <cyber-image class="pic" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg">
+            <cyber-image class="pic" src="/src/assets/img/cyber-building.jpeg">
                 <template #placeholder>
                     <div style="color: aqua;">具名插槽placeholder</div>
                 </template>
